609fac95b6948c38f435be2fe056e2f658c81d5e
MacPe-Dev <139461691+MacPe-Dev@users.noreply.github.com>
2023-07-18 11:53:37 +0100

    Update ej.js

--- ejercicio3/ej.js
+++ ejercicio3/ej.js
@@ -13,3 +13,12 @@ console.log("Ambos son iguales");
 else{
 console.log("El segundo número es el mayor");
 }
+// Revisar. Cuando pongo el primer numero mayor me sale el último mensaje.
+// Prueba esto
+// if (a==b) {
+//     console.log("El número " + a + " y " + "el número " + b + " son iguales");
+// }else if (a>b){
+//     console.log("El primero es mayor.")
+// } else if (b>a){
+//     console.log("El segundo es mayor")
+// }

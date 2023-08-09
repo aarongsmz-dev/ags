bc0f566f61946dbefc28d73a590a3c2867d7aa18
“aarongsmz-devgit config --global user.name “aarongsmz-dev <aarongsmz@gmail.comgit config --global user.email aarongsmz@gmail.com>
2023-08-09 19:32:20 +0100

    prueba

---
+++ PPF-UF1843/jquery.js
@@ -0,0 +1,43 @@
+$('#dos').click(function(){
+    $( '.cont2' ).toggle( "fade", 4000 );
+});
+
+$('#tres').click(function(){
+    $( '.cont3' ).toggle( "bounce", 4000 );
+});
+
+$('#cuatro').click(function(){
+    $( '.cont4' ).toggle( "clip", 4000 );
+});
+
+$('#cinco').click(function(){
+    $( '.cont5' ).toggle( "drop", 4000 );
+});
+
+$('#seis').click(function(){
+    $( '.cont6' ).toggle( "blind", 4000 );
+});
+
+$('#siete').click(function(){
+    $( '.cont7' ).toggle( "size", 4000 );
+});
+
+$('#ocho').click(function(){
+    $( '.cont8' ).toggle( "slide", 4000 );
+});
+
+$('#nueve').click(function(){
+    $( '.cont9' ).toggle( "scale", 4000 );
+});
+
+$('#diez').click(function(){
+    $( '.cont10' ).toggle( "pulsate", 4000 );
+});
+
+$('#once').click(function(){
+    $( '.cont11 ' ).toggle( "puff", 4000 );
+});
+
+$('#doce').click(function(){
+    $( '.cont12 ' ).toggle( "highlight", 4000 );
+});

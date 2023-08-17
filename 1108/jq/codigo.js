3e3244db0fcc423eadc0678c8ae50d9986aaa9e2
“aarongsmz-devgit config --global user.name “aarongsmz-dev <aarongsmz@gmail.comgit config --global user.email aarongsmz@gmail.com>
2023-08-17 18:40:52 +0100

    ags

--- 1108/jq/codigo.js
+++ 1108/jq/codigo.js
@@ -2,7 +2,7 @@
 document.querySelector("#contacto").addEventListener("click", function contacto(){
     document.querySelector('.contenedor__principal').innerHTML = `
        
-    <form action="htpps://regres.in/api/users" id="formulario" method="post">
+    <form action="htpps://regres.in/api/users" id="formulario" method="post" class="colorSwap">
 
                 <label for="">Nombre</label>
                 <input class="input-text" id="nombreAñadir" type="text" placeholder="Nombre">
@@ -29,7 +29,7 @@ document.querySelector("#contacto").addEventListener("click", function contacto(
 document.querySelector("#acceso").addEventListener("click", function contacto(){
        document.querySelector('.contenedor__principal').innerHTML = `
           
-       <form action="htpps://regres.in/api/users" id="inicio" method="post">
+       <form action="htpps://regres.in/api/users" id="inicio" method="post" class="colorSwap">
    
                    <label for="">Usuario</label>
                    <input class="input-text" id="asuntoAñadir" type="text" placeholder="Usuario">
@@ -46,7 +46,7 @@ document.querySelector("#acceso").addEventListener("click", function contacto(){
    document.querySelector("#registro").addEventListener("click", function contacto(){
     document.querySelector('.contenedor__principal').innerHTML = `
        
-    <form action="htpps://regres.in/api/users" id="inicio" method="post">
+    <form action="htpps://regres.in/api/users" id="inicio" method="post" class="colorSwap">
 
                 <label for="">Usuario</label>
                 <input class="input-text" id="asuntoAñadir" type="text" placeholder="Usuario">
@@ -67,10 +67,36 @@ document.querySelector("#acceso").addEventListener("click", function contacto(){
    document.querySelector("#informacion").addEventListener("click", function contacto(){
     document.querySelector('.contenedor__principal').innerHTML = `
 
-    <div class="back-info">
+    <div class="back-info class="colorSwap"">
     <h2 class="back-text" >Información</h2>
 
 <p class="back-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis, ullam impedit. Doloremque ipsam assumenda 
     quaerat amet impedit deserunt adipisci odit a consequuntur iusto nam aut alias magni, et fuga nemo.</p>
     </div>`
    })
+
+
+//    Cambio de color
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  var colorSwapButton = document.getElementById("colorSwapButton");
+
+  colorSwapButton.addEventListener("click", function() {
+    var elements = document.querySelectorAll(".colorSwap");
+    elements.forEach(function(element) {
+      element.style.backgroundColor = getRandomColor();
+    });
+  });
+
+  function getRandomColor() {
+    var letters = "0123456789ABCDEF";
+    var color = "#";
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+});
+
